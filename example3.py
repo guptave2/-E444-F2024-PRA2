1871700144f0c2cb58419b2a4d33f360236ce2fd
Vedant Gupta <vedant20gupta@gmail.com>
2024-09-22 14:10:49 -0400

    activity 1.3 done

---
+++ example3.py
@@ -0,0 +1,23 @@
+from flask_moment import Moment
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from datetime import datetime, timezone
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html', current_time=datetime.now(timezone.utc))
+
+
+@app.route('/user/<name>')
+def user(name):
+ return render_template('user.html', name=name)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
